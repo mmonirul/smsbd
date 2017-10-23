c85f08f7f4927531c4933caad9424fc192624bf8
Monirul Islam <mohammadmonirul@gmail.com>
2017-10-23 21:01:36 +0200

    first app commit

---
+++ practice/AspCoreAngular2/ClientApp/app/components/counter/counter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'counter',
+    templateUrl: './counter.component.html',
+    styleUrls: ['./style.scss']
+})
+export class CounterComponent {
+    public currentCount = 0;
+
+    public incrementCounter() {
+        this.currentCount++;
+    }
+}
