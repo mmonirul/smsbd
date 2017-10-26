f0d693161bf8c29015104ca1db1f95b3e3b859a6
Monirul Islam <mohammadmonirul@gmail.com>
2017-10-26 21:06:03 +0200

    added service folder

--- practice/AspCoreAngular2/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ practice/AspCoreAngular2/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -1,39 +1,47 @@
 import { Component, Inject } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { FetchDataServices } from '../../services/fetch-data.service';
 import * as models from '../../interfaces/common';
+
 
 @Component({
     selector: 'fetchdata',
-    templateUrl: './fetchdata.component.html'
+    templateUrl: './fetchdata.component.html',
+    providers: [FetchDataServices]
 })
 export class FetchDataComponent {
     public forecasts: models.WeatherForecast[];
-    public headers: any = null;
-
+    
     private jsonData: any = {};
 
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-
-        this.headers = new Headers();
-        this.headers.append('Content-Type', 'application/json');
-        //this.headers.append('Access-Control-Allow-Origin', '*');
+    constructor(http: Http, private _fetchDataService: FetchDataServices, @Inject('BASE_URL') baseUrl: string) {
 
         http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
             this.forecasts = result.json() as models.WeatherForecast[];
         }, error => console.error(error));
 
+        this.getData();
+        this.getJsonTypiCode();
+        //http.get('http://localhost:50008/api/trips').subscribe(result => {
+        //    console.log(result.json());
+        //}, error => console.error(error));
 
-        http.get('http://localhost:50008/api/trips').subscribe(result => {
-            console.log(result.json());
-        }, error => console.error(error));
+        //http.get('http://date.jsontest.com').subscribe(result => {
+        //    this.jsonData = result.json();
+        //    console.log(this.jsonData.time + ' - '+ this.jsonData.date);
+        //}, error => console.error(error));
+    }
 
-        http.get('http://date.jsontest.com', { headers: this.headers}).subscribe(result => {
-            this.jsonData = result.json();
-            console.log(this.jsonData.time + ' - '+ this.jsonData.date);
-        }, error => console.error(error));
+    getData(): void {
+        this._fetchDataService.getExternalData().subscribe(result => {
+            this.jsonData = result;
+            console.log(this.jsonData);
+        }, error => console.log(error))
+    }
 
-        
-
-
+    getJsonTypiCode(): void {
+        this._fetchDataService.getJsonTypicodeData().subscribe(result => {
+            console.log(result);
+        }, error => console.log(error))
     }
 }
---
+++ practice/AspCoreAngular2/ClientApp/app/services/fetch-data.service.ts
@@ -0,0 +1,25 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class FetchDataServices {
+
+    constructor(private http: Http) { }
+
+    getLocalHostData() {
+
+    };
+
+    getExternalData() {
+        return this.http.get('http://date.jsontest.com').map(response => response.json());
+    }
+
+    getJsonTypicodeData() {
+        return this.http.get('https://jsonplaceholder.typicode.com/users').map(res => res.json());
+    }
+
+
+}
